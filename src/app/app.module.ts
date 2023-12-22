c6f57100031e4113903acb2cd7f1f09e6a8054e8
Eduardo Franco <franc0edu24@gmail.com>
2023-12-22 18:26:56 -0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NavbarModule} from "./common/components/navbar/navbar.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NavbarModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
