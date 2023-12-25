dc2c80b5c5761452e727921755fed46e9785c34c
Eduardo Franco <franc0edu24@gmail.com>
2023-12-25 12:26:25 -0300

    Modal edit ok

--- src/app/pages/police-record/pages/police-record/police-record.component.ts
+++ src/app/pages/police-record/pages/police-record/police-record.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-police-record',
   templateUrl: './police-record.component.html',
-  styleUrl: './police-record.component.scss'
 })
 export class PoliceRecordComponent {
 
